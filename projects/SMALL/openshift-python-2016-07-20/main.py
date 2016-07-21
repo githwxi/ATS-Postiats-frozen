036dbba1d78ab78dd1ee9a4441c62462248eb041
Hongwei Xi <gmhwxi@gmail.com>
2016-07-21 01:20:01 -0400

    Updating: very very minorly

--- projects/SMALL/openshift-python-2016-07-20/main.py
+++ projects/SMALL/openshift-python-2016-07-20/main.py
@@ -5,9 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-# sys.path.append('./MYCODE')
-sys.path.append(os.getenv('OPENSHIFT_REPO_DIR', './')+'MYCODE')
-sys.path.append(os.getenv('OPENSHIFT_REPO_DIR', './')+'libatscc2py3')
+sys.path.append(os.path.join(app.root_path, 'MYCODE'))
+sys.path.append(os.path.join(app.root_path, 'libatscc2py3'))
+
 from hello_dats import hello
 
 @app.route('/')
