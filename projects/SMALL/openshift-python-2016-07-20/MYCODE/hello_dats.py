6b8e3ce96974e3146746c1bfd5472274d21e5daf
Hongwei Xi <gmhwxi@gmail.com>
2017-05-22 08:08:42 -0400

    Updating: fixing and reorganizing

--- projects/SMALL/openshift-python-2016-07-20/MYCODE/hello_dats.py
+++ projects/SMALL/openshift-python-2016-07-20/MYCODE/hello_dats.py
@@ -2,7 +2,7 @@
 ##
 ## The Python3 code
 ## is generated from ATS source by atscc2py3
-## The starting compilation time is: 2016-7-22: 16h:59m
+## The starting compilation time is: 2017-5-22:  8h: 8m
 ##
 ######
 
@@ -69,12 +69,12 @@ def generate_insult_1():
   tmp20 = ("baggage", tmp21)
   tmp19 = ("apple_join", tmp20)
   tmp28 = choice_0__0__1(tmp7)
-  tmp27 = ats2pypre_string_append(tmp28, " ")
+  tmp27 = ats2pypre_string_append_2(tmp28, " ")
   tmp35 = choice_0__0__2(tmp13)
-  tmp26 = ats2pypre_string_append(tmp27, tmp35)
-  tmp25 = ats2pypre_string_append(tmp26, " ")
+  tmp26 = ats2pypre_string_append_2(tmp27, tmp35)
+  tmp25 = ats2pypre_string_append_2(tmp26, " ")
   tmp42 = choice_0__0__3(tmp19)
-  tmpret6 = ats2pypre_string_append(tmp25, tmp42)
+  tmpret6 = ats2pypre_string_append_2(tmp25, tmp42)
   return tmpret6
 
 
@@ -89,7 +89,7 @@ def choice_0__0__1(arg0):
   #__patsflab_choice_0
   tmp1__1 = ats2pypre_list_length(arg0)
   tmp3__1 = ats2pypre_gt_int1_int1(tmp1__1, 0)
-  ats2pypre_assert_errmsg_bool1(tmp3__1, "/home/hwxi/Research/OPENSHIFT/insultapp/MYCODE/hello.dats: 408(line=32, offs=12) -- 425(line=32, offs=29)")
+  ats2pypre_assert_errmsg_bool1(tmp3__1, "/home/hwxi/Research/ATS-Postiats-frozen/projects/SMALL/openshift-python-2016-07-20/MYCODE/hello.dats: 475(line=37, offs=12) -- 492(line=37, offs=29)")
   tmp5__1 = ats2pypre_sub_int1_int1(tmp1__1, 1)
   tmp4__1 = ats2pylibc_random_randint(0, tmp5__1)
   tmpret0__1 = ats2pypre_list_get_at(arg0, tmp4__1)
@@ -107,7 +107,7 @@ def choice_0__0__2(arg0):
   #__patsflab_choice_0
   tmp1__2 = ats2pypre_list_length(arg0)
   tmp3__2 = ats2pypre_gt_int1_int1(tmp1__2, 0)
-  ats2pypre_assert_errmsg_bool1(tmp3__2, "/home/hwxi/Research/OPENSHIFT/insultapp/MYCODE/hello.dats: 408(line=32, offs=12) -- 425(line=32, offs=29)")
+  ats2pypre_assert_errmsg_bool1(tmp3__2, "/home/hwxi/Research/ATS-Postiats-frozen/projects/SMALL/openshift-python-2016-07-20/MYCODE/hello.dats: 475(line=37, offs=12) -- 492(line=37, offs=29)")
   tmp5__2 = ats2pypre_sub_int1_int1(tmp1__2, 1)
   tmp4__2 = ats2pylibc_random_randint(0, tmp5__2)
   tmpret0__2 = ats2pypre_list_get_at(arg0, tmp4__2)
@@ -125,7 +125,7 @@ def choice_0__0__3(arg0):
   #__patsflab_choice_0
   tmp1__3 = ats2pypre_list_length(arg0)
   tmp3__3 = ats2pypre_gt_int1_int1(tmp1__3, 0)
-  ats2pypre_assert_errmsg_bool1(tmp3__3, "/home/hwxi/Research/OPENSHIFT/insultapp/MYCODE/hello.dats: 408(line=32, offs=12) -- 425(line=32, offs=29)")
+  ats2pypre_assert_errmsg_bool1(tmp3__3, "/home/hwxi/Research/ATS-Postiats-frozen/projects/SMALL/openshift-python-2016-07-20/MYCODE/hello.dats: 475(line=37, offs=12) -- 492(line=37, offs=29)")
   tmp5__3 = ats2pypre_sub_int1_int1(tmp1__3, 1)
   tmp4__3 = ats2pylibc_random_randint(0, tmp5__3)
   tmpret0__3 = ats2pypre_list_get_at(arg0, tmp4__3)
@@ -140,8 +140,8 @@ def insult_5():
   tmplab_py = None
   #__patsflab_insult_5
   tmp51 = generate_insult_1()
-  tmp50 = ats2pypre_string_append("Thou ", tmp51)
-  tmpret49 = ats2pypre_string_append(tmp50, "!")
+  tmp50 = ats2pypre_string_append_2("Thou ", tmp51)
+  tmpret49 = ats2pypre_string_append_2(tmp50, "!")
   return tmpret49
 
 
@@ -153,10 +153,10 @@ def insult_name_6(arg0):
   funlab_py = None
   tmplab_py = None
   #__patsflab_insult_name_6
-  tmp54 = ats2pypre_string_append(arg0, ", thou ")
+  tmp54 = ats2pypre_string_append_2(arg0, ", thou ")
   tmp55 = generate_insult_1()
-  tmp53 = ats2pypre_string_append(tmp54, tmp55)
-  tmpret52 = ats2pypre_string_append(tmp53, "!")
+  tmp53 = ats2pypre_string_append_2(tmp54, tmp55)
+  tmpret52 = ats2pypre_string_append_2(tmp53, "!")
   return tmpret52
 
 
